40162b295cee24373327dfd7dd839daea22486d2
ra <r_a@lavabit.com>
2010-05-11 00:14:02 +0200

    make compression and password protection optional Signed-off-by: ra <r_a@lavabit.com>

--- kfilefactory_kde4.pl
+++ kfilefactory_kde4.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 # Name:			kfilefactory.pl (kde4 version)
 # Purpose:		easily upload a file or directory to filefactory.com
-# Version:		0.1.0
-# Date:			07.05.2010
+# Version:		0.1.1
+# Date:			11.05.2010
 # License:		GPLv2
 # Author:		Robert Annessi <robert@annessi.at>
 
@@ -12,27 +12,91 @@ use strict;
 use LWP::UserAgent;
 use File::Temp qw/ tempdir /;
 use File::Basename;
-use File::stat;
 use POSIX qw(floor);
 use HTTP::Request::Common qw(POST);
+use Fcntl ':mode';
 
 
+# global vars
 $HTTP::Request::Common::DYNAMIC_FILE_UPLOAD = 1;
+my $pwlen = 60;
 my $windowtitle = 'Share file';
 my $error;
+my $file_no = 1;
+my $file_compress = 2;
+my $file_password = 3;
+my $filename;
+my $filesize;
+my $req;
+my $password = "";
 
 
+# check for right number of arguments
 if ( $#ARGV != 0 ) {
-	print "Usage: $0 <file|directory>\n";
-	exit(0);
+  print "Usage: $0 <file|directory>\n";
+  exit(0);
 }
 
 
-# ask user to continue
-system("kdialog --title \"$windowtitle\" -yesno \"Do you want to upload \"$ARGV[0]\" to filefactory.com?\nIt will be compressed and password protected, but the protection should be considered weak.\"");
-if ( $? != 0 ) {
-	exit(1);
+sub errorchk($$) {
+  my ($ret, $txt) = (@_);
+  if ( $ret != 0 ) {
+    system("kdialog --title \"$windowtitle\" --error \"ERROR: $txt\"");
+    exit(1);
+  }
 }
+
+
+# check mode of file
+sub check_flag($$) {
+  my ($mode, $flag) = (@_);
+  if ($mode & $flag) { 
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+# check if argument is file or directory
+my $mode   = (stat($ARGV[0]))[2];
+# generate zip file if argument is directory
+if ($mode & S_IFDIR) {
+  $file_compress = 0;
+} elsif ($mode & S_IFREG) {
+  $file_compress = 2;
+} else {
+  print "\"$ARGV[0]\" is neither a file nor a directory!";
+  exit(3);
+}
+
+
+# ask user options
+if ($file_compress == 0) {
+  open (KDIALOG, "kdialog --title \"$windowtitle\" --radiolist \"Select options for uploading \"$ARGV[0]\" to filefactory.com:\" $file_compress \"Enable compression\" on $file_password \"Enable compression and (weak) password protection\" off |") or die $!;
+} else {
+  open (KDIALOG, "kdialog --title \"$windowtitle\" --radiolist \"Select options for uploading \"$ARGV[0]\" to filefactory.com:\" $file_no \"Do neither enable compression nor password protection\" on $file_compress \"Enable compression\" off $file_password \"Enable compression and (weak) password protection\" off |");
+}
+
+
+# evaluation of user options
+my @options;
+while (<KDIALOG>) {
+  @options = split(" ", $_);
+}
+close KDIALOG;
+# check return code of dialog
+errorchk($?, "Cancelled by user!");
+foreach my $element (@options) {
+  $element =~ s/\"//g;
+  if ($element == $file_compress) {
+    $file_compress = 0;
+  } elsif ($element == $file_password) {
+    $file_password = 0;
+  } elsif ($element == $file_no) {
+    $file_no = 0;
+  }
+}
+
 
 
 # initiate browser
@@ -41,59 +105,66 @@ $ua->agent("");    # do not identify
 $ua->requests_redirectable( [ 'GET', 'POST' ] );
 $ua->max_redirect(1);
 
-# create temporary directory (will automatically be removed when the script exits)
-my $tempdir = tempdir( "kfilefactory.XXXXXXXXXXXX", TMPDIR => 1, CLEANUP => 1 );
-
-# extract filename, path and suffix from argument
-( my $name, my $path, my $suffix ) = fileparse( "$ARGV[0]", "\.[^.]*" );
-my $namenoslash = "$name";
-$namenoslash =~ s/\\//g;
-$path =~ s/"//;
-$suffix =~ s/"//;
-
 # get start page from filefactory
 my $response = $ua->get('http://www.filefactory.com/');
 if ( !$response->is_success ) {
-	$error = $response->status_line;
-	system("kdialog --title \"$windowtitle\" --error \"ERROR: $error\"");
-	exit(1);
+  errorchk(1, "$response->status_line");
 }
 #print $response->content;
 
-
 # get upload url
 if ( $response->content !~ /<form accept-charset="UTF-8" id="uploader" action="(http:\/\/[a-z0-9]+\.filefactory\.com\/upload\.php)" method="post" enctype="multipart\/form-data">/) {
-	system("kdialog --title \"$windowtitle\" --error \"ERROR: Parsing website for upload form failed!\"");
-	exit(1);
+  errorchk(1, "Parsing website for upload form failed!");
 }
 my $URL = "$1";
 #print $response->content;
 
 
-# create zip file with random password
-my $password = "";
-$password = generatePassword(60);
-if ( $? != 0 ) {
-	system("kdialog --title \"$windowtitle\" --error \"ERROR: Changing directory!\"");
-	exit(1);
+# extract filename, path and suffix from argument
+( my $name, my $path, my $suffix ) = fileparse( "$ARGV[0]", "\.[^.]*" );
+$name =~ s/\\//g;
+$path =~ s/"//;
+$suffix =~ s/"//;
+
+# create zip file if requested
+if ($file_compress == 0 || $file_password == 0) {
+  # create temporary directory (will automatically be removed when the script exits)
+  my $tempdir = tempdir( "kfilefactory.XXXXXXXXXXXX", TMPDIR => 1, CLEANUP => 1 );
+
+  my $filename_nosuffix = "$name$suffix";
+  $filename = "$filename_nosuffix.zip";
+
+  chdir "$path" or die "$!";
+
+  # create zip file with random password
+  if ($file_password == 0) {
+    $password = generatePassword($pwlen);
+    system("zip -q -r -P \"$password\" \"$tempdir/$filename\" \"$filename_nosuffix\"");
+    errorchk($?, "Creating zip file failed!");
+
+  # create zip file without password
+  } else {
+    system("zip -q -r \"$tempdir/$filename\" \"$filename_nosuffix\"");
+    errorchk($?, "Creating zip file failed!");
+  }
+  $filesize = (stat("$tempdir/$filename"))[7];
+  $req = POST("$URL", [ 'redirect' => '1', 'file' => [ "$tempdir/$filename" ] ], 'content_type' => 'multipart/form-data');
+
+
+# neither compression nor password protection
+} else {
+  $filename = "$name$suffix";
+  $filesize = (stat("$path$filename"))[7];
+  $req = POST("$URL", [ 'redirect' => '1', 'file' => [ "$path$filename" ] ], 'content_type' => 'multipart/form-data');
 }
-chdir "$path" or die "$!";
-system("zip -q -r -P \"$password\" \"$tempdir/$name$suffix.zip\" \"$name$suffix\"");
-if ( $? != 0 ) {
-	system("kdialog --title \"$windowtitle\" --error \"ERROR: Creating zip file failed!\"");
-	exit(1);
-}
-my $filesize = stat("$tempdir/$namenoslash$suffix.zip")->size;
-my $req = POST("$URL", [ 'redirect' => '1', 'file' => [ "$tempdir/$namenoslash$suffix.zip" ] ], 'content_type' => 'multipart/form-data');
 
 
 # create progressbar dialog
-my $dbusRef=`kdialog --title \"$windowtitle\" --progressbar "Uploading $name$suffix.zip ..." 100`;
+my $dbusRef=`kdialog --title \"$windowtitle\" --progressbar "Uploading $filename ..." 100`;
 if ( $dbusRef =~ /(org\.kde\.kdialog-[0-9]+) \/ProgressDialog/ ) {
-	$dbusRef = "$1";
+  $dbusRef = "$1";
 } else {
-	system("kdialog --title \"$windowtitle\" --error \"ERROR: Getting dbus reference failed!\"");
-	exit(1);
+  errorchk(1, "ERROR: Getting dbus reference failed!");
 }
 system("qdbus $dbusRef /ProgressDialog showCancelButton true");
 
@@ -101,25 +172,23 @@ system("qdbus $dbusRef /ProgressDialog showCancelButton true");
 # update progress bar
 my $gen = $req->content;
 if (ref($gen) ne 'CODE') {
-	system("kdialog --title \"$windowtitle\" --error \"ERROR: Uploading file in chunks!\"");
-	exit(1);
+  errorchk(1, "ERROR: Uploading file in chunks!");
 }
 my $total;
 my $percent;
 
 $req->content(
-	sub {
-		my $chunk = &$gen();
-		$total += length($chunk) if defined $chunk;
-		$percent = floor($total/$filesize*100);
-		if ( `qdbus $dbusRef /ProgressDialog wasCancelled` eq "true\n" ) {
-			system("qdbus $dbusRef /ProgressDialog close");
-			system("kdialog --title \"$windowtitle\" --error \"Upload cancelled!\"");
-			exit(1);
-		}
-		system("qdbus $dbusRef /ProgressDialog Set \"\" value $percent");
-		return $chunk;
-	}
+  sub {
+    my $chunk = &$gen();
+    $total += length($chunk) if defined $chunk;
+    $percent = floor($total/$filesize*100);
+    if ( `qdbus $dbusRef /ProgressDialog wasCancelled` eq "true\n" ) {
+      system("qdbus $dbusRef /ProgressDialog close");
+      errorchk(1, "ERROR: Upload cancelled!");
+    }
+    system("qdbus $dbusRef /ProgressDialog Set \"\" value $percent");
+    return $chunk;
+  }
 );
 
 
@@ -127,21 +196,20 @@ $req->content(
 $response = $ua->request($req);
 system("qdbus $dbusRef /ProgressDialog close");
 if ( !$response->is_success ) {
-	$error = $response->status_line;
-	system("kdialog --title \"$windowtitle\" --error \"ERROR: $error\"");
-	exit(1);
+  errorchk(1, "$response->status_line");
 }
 #print $response->content;
 
 
 # get url for uploaded file
 if ( $response->content =~ /(http:\/\/www\.filefactory\.com\/file\/[a-z0-9]+\/n\/[A-Za-z0-9_]+)/ ) {
-	system("kdialog --title \"$windowtitle\" --msgbox \"File upload successful!\n
-	URL: $1\n
-	Password: $password\"");
+  if ($file_password == 0) {
+    system("kdialog --title \"$windowtitle\" --msgbox \"\nFile upload successful!\nURL: $1\nPassword: $password\n\"");
+  } else {
+    system("kdialog --title \"$windowtitle\" --msgbox \"\nFile upload successful!\nURL: $1\\n\"");
+  }
 } else {
-	system("kdialog --title \"$windowtitle\" --error \"ERROR: Getting URL for uploaded file failed!\"");
-	exit(1);
+  errorchk(1, "Getting URL for uploaded file failed");
 }
 #print $response->content;
 
@@ -150,11 +218,11 @@ exit(0);
 
 
 sub generatePassword {
-	my $length = shift;
-	my $possible = 'abcdefghijklmnopqrstuvwxyz1234567780ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	while ( length($password) < $length ) {
-		$password .= substr( $possible, ( int( rand( length($possible) ) ) ), 1 );
-	}
-	return $password;
+  my $length = shift;
+  my $possible = 'abcdefghijklmnopqrstuvwxyz1234567780ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  while ( length($password) < $length ) {
+    $password .= substr( $possible, ( int( rand( length($possible) ) ) ), 1 );
+  }
+  return $password;
 }
 
